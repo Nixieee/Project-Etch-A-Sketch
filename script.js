2e56507ccc79a17453ed541e738f638587ddac5f
Nixieee <niki.010821@gmail.com>
2022-05-05 19:16:10 +0300

    Remaked grid

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
 //Creating a grid
-
 const grid = document.querySelector('.grid');
-grid.innerHTML =
-    `<div class="row">${'<div class="cell"></div>'.repeat(16)}</div>`
-        .repeat(16);
-        drawing();
-//Handling drawing
-function drawing(){
-const cell = grid.querySelectorAll('.cell');
-console.log(cell);
-let isDrawing = false;
-cell.forEach(element => element.addEventListener("mousedown", event => {
-    element.style.backgroundColor = "black";
-    isDrawing = true;
-    cell.forEach(element => element.addEventListener("mouseover", event => {
-        if(isDrawing === true){
-        element.style.backgroundColor = "black";
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add("cell")
+        grid.appendChild(gridElement)
     }
+    drawing();
+}
+
+//Handling drawing
+function drawing() {
+    const cell = grid.querySelectorAll('.cell');
+    console.log(cell);
+    let isDrawing = false;
+    cell.forEach(element => element.addEventListener("mousedown", event => {
+        element.style.backgroundColor = "black";
+        isDrawing = true;
+        cell.forEach(element => element.addEventListener("mouseover", event => {
+            if (isDrawing === true) {
+                element.style.backgroundColor = "black";
+            }
+        }));
     }));
-}));
-cell.forEach(element => element.addEventListener("mouseup", event => {
-isDrawing = false;
-}));
+    cell.forEach(element => element.addEventListener("mouseup", event => {
+        isDrawing = false;
+    }));
 }
 
 
 //Grid size slider
 var slider = document.getElementById("myRange");
 var output = document.getElementById("rangeValue");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerHTML = slider.value + "x" + slider.value; // Display the default slider value
+setupGrid(slider.value);
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-  let i = 0, n = this.value;
-  grid.innerHTML =
-    `<div class="row">${'<div class="cell"></div>'.repeat(n)}</div>`
-        .repeat(n);
-        drawing();
-
+slider.oninput = function () {
+    output.innerHTML = this.value + "x" + this.value;
+    grid.innerHTML = "";
+    setupGrid(this.value);
 }
 
 
